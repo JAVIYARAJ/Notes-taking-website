82c6bf5c8a93bee1d1b820fa53cf3479004af497
javiyaraj <javiyaraj4@gmail.com>
2021-07-29 21:48:40 +0530

    add title box in top of the website

--- index.js
+++ index.js
@@ -1,71 +1,73 @@
-console.log('project1');
 showNotes();
 
-// if user click on add note then your note add into localstorage.
-
+// code for add a note in local strorage and after add display all notes.
 let addBtn = document.getElementById("addBtn");
-addBtn.addEventListener('click', function(e) {
+addBtn.addEventListener("click", function(e) {
   let addTxt = document.getElementById("addTxt");
+  let addTitle = document.getElementById("addTitle");
   let notes = localStorage.getItem("notes");
   if (notes == null) {
-    notesObj = []; 
+    notesObj = [];
   } else {
     notesObj = JSON.parse(notes);
-    console.log(notesObj);
   }
-  notesObj.push(addTxt.value);
+  let myObj = {
+    title: addTitle.value,
+    text: addTxt.value
+  }
+  notesObj.push(myObj);
   localStorage.setItem("notes", JSON.stringify(notesObj));
   addTxt.value = "";
+  addTitle.value = "";
   showNotes();
 });
 
-// show function
+// code for display a all notes
 function showNotes() {
-    let notes = localStorage.getItem("notes");
-    if (notes == null) {
-      notesObj = [];
-    } else {
-      notesObj = JSON.parse(notes);
-    }
-    let html = "";
-    notesObj.forEach(function(element, index) {
-      html += `
-              <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
-                      <div class="card-body">
-                          <h5 class="card-title">Note ${index + 1}</h5>
-                          <p class="card-text"> ${element}</p>
-                          <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  let html = "";
+  notesObj.forEach(function(element, index) {
+    html += `
+            <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">${element.title}</h5>
+                        <p class="card-text"> ${element.text}</p>
+                        <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
+                    </div>
+                </div>`;
+  });
+  let notesElm = document.getElementById("notes");
+  if (notesObj.length != 0) {
+    notesElm.innerHTML = html;
+  } else {
+    notesElm.innerHTML = `Nothing to show! Use "Add a Note" section above to add notes.`;
+  }
+}
 
-                      </div>
-                  </div>`;
-    });
-    let notesElm = document.getElementById("notes");
-    if (notesObj.length != 0) {
-      notesElm.innerHTML = html;
-    } else {
-      notesElm.innerHTML = `<h3>Note is not avaliable plz add a notes!</h3>`;
-    }
+// code for delete a note
+function deleteNote(index) {
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
   }
 
-  // Function to delete a note
-function deleteNote(index) {
-    //   console.log("I am deleting", index);
-    
-      let notes = localStorage.getItem("notes");
-      if (notes == null) {
-        notesObj = [];
-      } else {
-        notesObj = JSON.parse(notes);
-      }
-    
-      notesObj.splice(index, 1);
-      localStorage.setItem("notes", JSON.stringify(notesObj));
-      showNotes();
-    }
+  notesObj.splice(index, 1);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  showNotes();
+}
 
-    //search perticular notes
-    let search = document.getElementById('searchTxt');
-    search.addEventListener("input", function(){
+
+
+// code for search a element
+let search = document.getElementById('searchTxt');
+search.addEventListener("input", function(){
 
     let inputVal = search.value.toLowerCase();
     // console.log('Input event fired!', inputVal);
@@ -78,6 +80,6 @@ function deleteNote(index) {
         else{
             element.style.display = "none";
         }
-        // console.log(cardTxt);
+        
     })
 })
